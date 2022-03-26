c9d7ee4b4b00bbb74da3817666d58a36662ec2e0
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2022-03-26 16:01:56 +0200

    repo configuration

---
+++ tasks.py
@@ -0,0 +1,27 @@
+from invoke import task
+
+@task
+def start(ctx):
+	ctx.run("python3 src/main.py")
+
+@task
+def coverage(ctx):
+	ctx.run("coverage run --branch -m pytest src")
+
+@task
+def coverage_report(ctx):
+	ctx.run("coverage html")
+
+@task
+def lint(ctx):
+	ctx.run("pylint src")
+
+@task
+def test(ctx):
+	ctx.run("pytest src")
+
+@task
+def format(ctx):
+	ctx.run("autopep8 --in-place --recursive src")
+
+
